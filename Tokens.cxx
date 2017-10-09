cecc0710183d253ed5ef6de11ae9e713f24b5f39
c4explosive <ao946391@gmail.com>
2017-10-09 01:38:36 -0500

    Automatas prototypes

--- Tokens.cxx
+++ Tokens.cxx
@@ -20,3 +20,115 @@ Lexema * Tokens::getLx()
 {
     return this->lx;
 }
+
+void Tokens::autoManager()
+{
+    int flag=0;
+    flag=autoReserv();
+    if(flag) 
+	return;
+    flag=autoAgrup();
+    if(flag)
+	return;
+    flag=autoArit();
+    if(flag)
+	return;
+    flag=autoAsig();
+    if(flag)
+	return;
+    flag=autoComp();
+    if(flag)
+	return;
+    flag=autoFLine();
+    if(flag)
+	return;
+    flag=autoComment();
+    if(flag)
+	return;
+    flag=autoDigits();
+    if(flag)
+	return;
+   
+}
+
+int Tokens::autoReserv()
+{
+    return 1;
+}
+
+int Tokens::autoAgrup()
+{
+    return 1;
+}
+
+int Tokens::autoArit()
+{
+    return 1;
+}
+
+int Tokens::autoComp()
+{
+    return 1;
+}
+
+int Tokens::autoFLine()
+{
+    return 1;
+}
+
+int Tokens::autoAsig()
+{
+    return 1;
+}
+
+int Tokens::autoComment()
+{
+    return 1;
+}
+
+int Tokens::autoDigits()
+{
+    return 1;
+}
+
+void Tokens::whatType()
+{
+    int type;
+    type=lx->getType();
+    cout<<"El lexema es: "<<lx->getLex();
+    switch(type)
+    {
+	case PALABRA_RESERVADA:
+       		cout<<" -> Palabra reservada"<<endl;
+		break;
+	case AGRUPACION:
+       		cout<<" -> Signos de arupacion"<<endl;
+		break;
+	case OPER_AR:
+		cout<<" -> Operador Aritmetico"<<endl;
+		break;
+	case OPER_ASIG:
+		cout<<" -> Operador de Asignacion"<<endl;
+		break;
+	case OPER_COMP:
+		cout<<" -> Operador de Comparacion"<<endl;
+		break;
+	case FINAL_LINEA:
+		cout<<" -> Final de linea"<<endl;
+		break;
+	case COMENTARIO:
+		cout<<" -> Comentario"<<endl;
+		break;
+	case DIGITOS:
+		cout<<" -> Digitos/Numeros"<<endl;
+		break;
+	case INVALIDO:
+		cout<<" -> Error Lexico // Token no encontrado"<<endl;
+		break;
+	default:
+		cout<<" -> Esto es mas que desconocido..."<<endl;
+		break;
+
+    }
+}
+
--- Tokens.h
+++ Tokens.h
@@ -1,13 +1,25 @@
 #include "Lexema.h"
 class Tokens
 {
+private:
     int nPR;
     Lexema *lx;
-    public:
+    int autoReserv();
+    int autoAgrup();
+    int autoArit();
+    int autoAsig();
+    int autoComp();
+    int autoFLine();
+    int autoComment();
+    int autoDigits();
+
+public:
     enum tTokens { PALABRA_RESERVADA, AGRUPACION, OPER_AR, OPER_ASIG, OPER_COMP, FINAL_LINEA, COMENTARIO, DIGITOS, INVALIDO};
     char ** paReserv;
     Tokens();
     void setLx(Lexema * lx);
     Lexema * getLx();
+    void autoManager();
+    void whatType();
 
 };
